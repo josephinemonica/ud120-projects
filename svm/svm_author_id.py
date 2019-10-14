020aeffb91f533153668efc6b6bb0fef4e181ec6
Josephine Monica <josephinemonica09@gmail.com>
2019-10-14 08:17:15 -0400

    Finished SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+# Create classifier and train
+clf = SVC(kernel='rbf',C=10000)
+# train with smaller training set to speed up computation
+#clf.fit(features_train[:len(features_train)/100],labels_train[:len(features_train)/100])
+clf.fit(features_train,labels_train)
+
+# Predict
+predicted_train = clf.predict(features_train)
+predicted_test = clf.predict(features_test)
+
+# Accuracy
+acc_train = accuracy_score(labels_train,predicted_train)
+acc_test = accuracy_score(labels_test,predicted_test)
+
+print("Training accuracy is {}".format(acc_train))
+print("Test accuracy is {}".format(acc_test))
 #########################################################
 
 
