a2c73619da307d754b7204c567a0be12b00200f4
Josephine Monica <josephinemonica09@gmail.com>
2019-11-04 19:08:27 -0500

    Finished dataset questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,33 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
+# How many folks in this dataset have a quantified salary? What about a known email address?
+n_email_address = 0
+n_salary = 0
 
+# How many folks in this datasetwith NaN in total payments
+nan_total_payments = 0
+
+# how many POIs have NaN total payments
+# What percentage of POI as a whole is this
+n_POI = 0
+nan_total_payments_POI = 0
+
+for key in enron_data.keys():
+    if(enron_data[key]['email_address'] != 'NaN'):
+        n_email_address = n_email_address+1
+    if(enron_data[key]['salary'] != 'NaN'):
+        n_salary = n_salary+1
+    if(enron_data[key]['total_payments']=='NaN'):
+        nan_total_payments += 1
+    if(enron_data[key]['poi']):
+        n_POI += 1
+        if(enron_data[key]['total_payments']=='NaN'):
+            nan_total_payments_POI += 1
+            
+print("No of people w quantified email is {}".format(n_email_address))
+print("No of people w quantified salary is {}".format(n_salary))
+print("{} out of {} people in this dataset have NaN total payments".\
+      format(nan_total_payments, len(enron_data.keys())))
+print("{} out of {} POI have NaN total payments".\
+      format(nan_total_payments_POI, n_POI))
