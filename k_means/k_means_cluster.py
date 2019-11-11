a7f6c4b16aa89fd4f22085e8a214d68b4f1c62fc
Josephine Monica <josephinemonica09@gmail.com>
2019-11-10 20:46:06 -0500

    Finished KMeans

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -43,13 +43,25 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
+'''
+'''
+USE_TOTAL_PAYMENTS_FEATURE = False
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+
+# Add a new feature of total payments
+feature_3 = "total_payments"
+
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+
+if(USE_TOTAL_PAYMENTS_FEATURE):
+    features_list = [poi, feature_1, feature_2, feature_3]
+else:
+    features_list = [poi, feature_1, feature_2]
+
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,13 +70,22 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
-plt.show()
 
+if(USE_TOTAL_PAYMENTS_FEATURE):
+    for f1, f2, f3 in finance_features:
+        plt.scatter( f1, f2 )
+    plt.show()
+else:
+    for f1, f2 in finance_features:
+        plt.scatter( f1, f2 )
+    plt.show()
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
 
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(finance_features)
+pred = kmeans.predict(finance_features)
 
 
 
@@ -74,3 +95,18 @@ try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
+
+'''
+'''
+# From queying the data dictionary, what's the max and min values taken by
+# exercised_stock_options (ignore NaN)
+exercised_stock_options_list = [data_dict[key]["exercised_stock_options"] for key in data_dict.keys() if data_dict[key]["exercised_stock_options"]!='NaN']
+print("Max of exercised_stock_options is {}".format(max(exercised_stock_options_list)))
+print("Min of exercised_stock_options is {}".format(min(exercised_stock_options_list)))
+
+# From queying the data dictionary, what's the max and min values taken by
+# salary (ignore NaN)
+salary_list = [data_dict[key]["salary"] for key in data_dict.keys() if data_dict[key]["salary"]!='NaN']
+print("Max of salary is {}".format(max(salary_list)))
+print("Min of salary is {}".format(min(salary_list)))
+
